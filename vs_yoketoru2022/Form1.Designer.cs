9950bd5f8f1094555c4ddfef82db2444b0f68756
A061-09 <tnk.dat.class@gmail.com>
2022-07-14 16:12:49 +0900

    Initial commit

---
+++ vs_yoketoru2022/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace vs_yoketoru2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeL = new System.Windows.Forms.Label();
+            this.scoreL = new System.Windows.Forms.Label();
+            this.titieBt = new System.Windows.Forms.Button();
+            this.crL = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.overBt = new System.Windows.Forms.Button();
+            this.overL = new System.Windows.Forms.Label();
+            this.titieLb = new System.Windows.Forms.Label();
+            this.clearL = new System.Windows.Forms.Label();
+            this.overLb = new System.Windows.Forms.Label();
+            this.tempL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeL
+            // 
+            this.timeL.AutoSize = true;
+            this.timeL.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timeL.Location = new System.Drawing.Point(662, 9);
+            this.timeL.Name = "timeL";
+            this.timeL.Size = new System.Drawing.Size(54, 28);
+            this.timeL.TabIndex = 2;
+            this.timeL.Text = "Time";
+            // 
+            // scoreL
+            // 
+            this.scoreL.AutoSize = true;
+            this.scoreL.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.scoreL.Location = new System.Drawing.Point(12, 9);
+            this.scoreL.Name = "scoreL";
+            this.scoreL.Size = new System.Drawing.Size(58, 28);
+            this.scoreL.TabIndex = 3;
+            this.scoreL.Text = "score";
+            // 
+            // titieBt
+            // 
+            this.titieBt.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titieBt.Location = new System.Drawing.Point(369, 293);
+            this.titieBt.Name = "titieBt";
+            this.titieBt.Size = new System.Drawing.Size(66, 41);
+            this.titieBt.TabIndex = 4;
+            this.titieBt.Text = "start";
+            this.titieBt.UseVisualStyleBackColor = true;
+            this.titieBt.Click += new System.EventHandler(this.titleBt_Click);
+            // 
+            // crL
+            // 
+            this.crL.AutoSize = true;
+            this.crL.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.crL.Location = new System.Drawing.Point(273, 413);
+            this.crL.Name = "crL";
+            this.crL.Size = new System.Drawing.Size(259, 28);
+            this.crL.TabIndex = 5;
+            this.crL.Text = "copyright©2022　○○○○";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // overBt
+            // 
+            this.overBt.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.overBt.Location = new System.Drawing.Point(369, 293);
+            this.overBt.Name = "overBt";
+            this.overBt.Size = new System.Drawing.Size(66, 41);
+            this.overBt.TabIndex = 6;
+            this.overBt.Text = "title";
+            this.overBt.UseVisualStyleBackColor = true;
+            // 
+            // overL
+            // 
+            this.overL.AutoSize = true;
+            this.overL.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.overL.Location = new System.Drawing.Point(355, 203);
+            this.overL.Name = "overL";
+            this.overL.Size = new System.Drawing.Size(0, 37);
+            this.overL.TabIndex = 8;
+            // 
+            // titieLb
+            // 
+            this.titieLb.AutoSize = true;
+            this.titieLb.Font = new System.Drawing.Font("Yu Gothic UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titieLb.Location = new System.Drawing.Point(284, 121);
+            this.titieLb.Name = "titieLb";
+            this.titieLb.Size = new System.Drawing.Size(226, 46);
+            this.titieLb.TabIndex = 9;
+            this.titieLb.Text = "Yoketoru2022";
+            // 
+            // clearL
+            // 
+            this.clearL.AutoSize = true;
+            this.clearL.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clearL.Location = new System.Drawing.Point(355, 220);
+            this.clearL.Name = "clearL";
+            this.clearL.Size = new System.Drawing.Size(94, 37);
+            this.clearL.TabIndex = 10;
+            this.clearL.Text = "CLEAR";
+            // 
+            // overLb
+            // 
+            this.overLb.AutoSize = true;
+            this.overLb.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.overLb.Location = new System.Drawing.Point(326, 220);
+            this.overLb.Name = "overLb";
+            this.overLb.Size = new System.Drawing.Size(158, 37);
+            this.overLb.TabIndex = 11;
+            this.overLb.Text = "GAMEOVER";
+            // 
+            // tempL
+            // 
+            this.tempL.AutoSize = true;
+            this.tempL.Location = new System.Drawing.Point(89, 183);
+            this.tempL.Name = "tempL";
+            this.tempL.Size = new System.Drawing.Size(38, 15);
+            this.tempL.TabIndex = 12;
+            this.tempL.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tempL);
+            this.Controls.Add(this.overLb);
+            this.Controls.Add(this.clearL);
+            this.Controls.Add(this.titieLb);
+            this.Controls.Add(this.overL);
+            this.Controls.Add(this.overBt);
+            this.Controls.Add(this.crL);
+            this.Controls.Add(this.titieBt);
+            this.Controls.Add(this.scoreL);
+            this.Controls.Add(this.timeL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label timeL;
+        private System.Windows.Forms.Label scoreL;
+        private System.Windows.Forms.Button titieBt;
+        private System.Windows.Forms.Label crL;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button overBt;
+        private System.Windows.Forms.Label overL;
+        private System.Windows.Forms.Label titieLb;
+        private System.Windows.Forms.Label clearL;
+        private System.Windows.Forms.Label overLb;
+        private System.Windows.Forms.Label tempL;
+    }
+}
+
---
+++ vs_yoketoru2022/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace vs_yoketoru2022
+{
+    public partial class Form1 : Form
+    {
+        const bool isDebug = true;
+
+        const int SpeedMax = 10;
+        const int PlayerMax = 1;
+        const int EnemyMax = 10;
+        const int ItemMax = 10;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] Chrs = new Label[ChrMax];
+        int[] vx = new int[ChrMax];
+        int[] vy = new int[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;
+        const int ItemIndex = EnemyIndex + EnemyMax;
+
+        const string PlayerText = "○";
+        const string EnemyText = "▲";
+        const string ItemText = "□";
+
+        static Random rand=new Random();
+
+        enum State
+        {
+            None = -1,
+            Title,
+            Game,
+            Gameover,
+            Clear,
+        }
+        State currentState = State.None;
+        State nextState = State.Title;
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < ChrMax; i++)
+            {
+                Chrs[i] = new Label();
+                Chrs[i].AutoSize = true;
+                if (i == PlayerIndex)
+                {
+                    Chrs[i].Text = PlayerText;
+                }
+                else if (i < ItemIndex)
+                {
+                    Chrs[i].Text = EnemyText;
+                }
+                else
+                {
+                    Chrs[i].Text = ItemText;
+                }
+                Chrs[i].Font = tempL.Font;
+                Controls.Add(Chrs[i]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nextState != State.None)
+            {
+                initProc();
+            }
+
+            if (isDebug)
+            {
+                if (GetAsyncKeyState((int)Keys.O) < 0)
+                {
+                    nextState = State.Gameover;
+                }
+                else if (GetAsyncKeyState((int)Keys.C) < 0)
+                {
+                    nextState = State.Clear;
+                }
+            }
+
+            if (currentState == State.Game)
+            {
+                updateGames();
+            }
+        }
+
+        void updateGames()
+        {
+            Point mp = PointToClient(MousePosition);
+            /*
+            ChrMax[PlayerIndex].Left = mp.X - ChrMax[PlayerIndex].Width / 2;
+            ChrMax[PlayerIndex].Top = mp.X - ChrMax[PlayerIndex].Height / 2;
+            */
+            for(int i = EnemyIndex;i<ChrMax;i++)
+            {
+                Chrs[i].Left += vx[i];
+                Chrs[i].Top += vy[i];
+            }
+        }
+
+        void initProc()
+        {
+            currentState = nextState;
+            nextState = State.None;
+
+            switch (currentState)
+            {
+                case State.Title:
+                    titieLb.Visible = true;
+                    titieBt.Visible = true;
+                    crL.Visible = true;
+                    scoreL.Visible = false;
+                    timeL.Visible = false;
+                    overBt.Visible = false;
+                    clearL.Visible = false;
+                    overLb.Visible = false;
+                    break;
+            }
+        }
+
+        private void titleBt_Click(object sender, EventArgs e)
+        {
+            nextState = State.Game;
+        }
+        private void overBt_click(object sender, EventArgs e)
+        {
+            nextState = State.Title;
+        }
+    }
+}
